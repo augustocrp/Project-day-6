063a423b6904620eb78ebb72771841d6058d1a73
Augusto Cesar <dev.augusto.crp@gmail.com>
2022-12-22 23:47:35 -0300

    Update termoo.py

--- termoo.py
+++ termoo.py
@@ -4,3 +4,9 @@ import random
 f = open("word.json", encoding="utf8")
 
 words = json.load(f)
+choice_c = random.choice(list(words.keys()))
+
+print ("Olá, seja bem vindo!")
+print ("########################")
+
+answer_user = input("Data: DDMMAAAA\n")
